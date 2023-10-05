528c9a0d4b0de79430cc0a3ec28a89d09bb37385
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-10-04 21:55:38 -0400

    Implement prepend and size method

--- main.rb
+++ main.rb
@@ -6,6 +6,14 @@ class LinkedList
     def append(node)
         @linked_list << node.value
     end
+
+    def prepend(node)
+        @linked_list.unshift node.value
+    end
+
+    def size
+        @linked_list.size
+    end
 end
 
 class Node
