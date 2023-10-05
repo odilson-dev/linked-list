27aec7c6046fbad594651839935c254e61ff603b
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-10-04 21:52:39 -0400

    Add append method to LinkedList class

--- main.rb
+++ main.rb
@@ -1,9 +1,15 @@
 class LinkedList
     def initialize
+        @linked_list = []
+    end
+
+    def append(node)
+        @linked_list << node.value
     end
 end
 
 class Node
+    attr_accessor :value
     def initialize
         @value = nil
         @next_node = nil
